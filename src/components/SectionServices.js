8642bebfb30bb0325563b0b27aafb723e36b2319
avidler <a.vidler@live.co.uk>
2020-05-31 13:02:22 +0100

    update services section

--- src/components/SectionServices.js
+++ src/components/SectionServices.js
@@ -11,26 +11,8 @@ export default class SectionHero extends React.Component {
             <section id={_.get(section, 'section_id')} className="block hero-block bg-accent outer">
               <div className="inner">
                 <div className="grid">
-                  {_.get(section, 'image') && 
-                  <div className="cell block-preview">
-                    <img src={safePrefix(_.get(section, 'image'))} alt={_.get(section, 'title')} />
-                  </div>
-                  }
-                  <div className="cell block-content">
-                    {_.get(section, 'title') && 
-                    <h2 className="block-title underline">{_.get(section, 'title')}</h2>
-                    }
-                    <div className="block-copy">
-                      {markdownify(_.get(section, 'content'))}
-                    </div>
-                    {_.get(section, 'actions') && 
-                    <p className="block-buttons">
-                      {_.map(_.get(section, 'actions'), (action, action_idx) => (
-                        <ActionLink key={action_idx} {...this.props} action={action} class_names={'button white large'} />
-                      ))}
-                    </p>
-                    }
-                  </div>
+                    <p>Services Section</p>
+
                 </div>
               </div>
             </section>

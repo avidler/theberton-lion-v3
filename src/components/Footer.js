f41c3dbbce44b664460193d7c4e74d20af659a5e
Adam Vidler <a.vidler@live.co.uk>
2020-06-11 21:44:13 +0100

    footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,17 +9,17 @@ export default class Footer extends React.Component {
         return (
             <footer id="colophon" className="site-footer">
               <div className="container">
-                <div className="footer_address">
+                <div className="footer_section footer_address">
                   <h5>contact/directions</h5>
-                  <strong>Address:</strong>
+                  <strong className="footer_heading">Address:</strong>
                   <p>Theberton, Leiston, Suffolk , IP16 4RU</p>
-                  <strong>Phone / Email: </strong>
+                  <strong className="footer_heading">Phone / Email: </strong>
                   <p>01728 830185</p>
                   <a title="Email Theberton Lion" href="mailto:bookings@thebertonlion.co.uk">bookings@thebertonlion.co.uk</a>
-			            <strong>Directions: </strong>
+			            <strong className="footer_heading">Directions: </strong>
                   <a title="Get Directions to the Theberton Lion Pub" href="http://www.thebertonlion.co.uk/#">Click here for directions</a>	
                 </div>
-                <div className="footer_opening_hours">
+                <div className="footer_section footer_opening_hours">
                   <h5>Our Opening Hours</h5>
                   <div class="openingHours">
                     <p>Monday <span>12.00pm – 3.00pm | 6.00pm – 11.00pm</span></p>
@@ -31,12 +31,12 @@ export default class Footer extends React.Component {
                     <p>Sunday <span>12.00pm – 3.00pm | 7.00pm – 11.00pm</span></p>
                   </div>
                 </div>
-                <div className="footer_logo">
+                <div className="footer_section footer_logo">
                 {_.get(this.props, 'pageContext.site.siteMetadata.header.logo_img') && 
                     <p className="site-logo">
-                      <Link to={safePrefix('/')}>
+                      
                         <img className="larger" id="site-logo-img" src={safePrefix(_.get(this.props, 'pageContext.site.siteMetadata.header.logo_img'))} alt="Logo" />
-                      </Link>
+                     
                     </p>
                     }
                 </div>

cb0400561dacc65812ecbee06a39adb4b73dec3a
Adam Vidler <a.vidler@live.co.uk>
2020-07-07 16:15:07 +0100

    added beer list template

--- src/templates/drinkslist.js
+++ src/templates/drinkslist.js
@@ -7,11 +7,14 @@ export default class DrinksList extends React.Component {
      
     render() {
         const beerList = _.get(this.props, 'pageContext.site.data.beer.beers')
-        const beerOnNow = beerList.filter(a => a.beer_on_now === true)
-        const beerComingSoon = beerList.filter(a => a.beer_coming_soon === true)
-        console.log("beerList",beerList)
+        console.log("beerList", beerList)
+        const beerOnNow = beerList.filter(a => a.beer_on_now)
+        const beerComingSoon = beerList.filter(a => a.beer_coming_soon)
+        console.log("beerOnNow",beerOnNow)
+        console.log("beerComingSoon",beerComingSoon)
         return (
             <Layout {...this.props}>
+            <div className="beer_on_now">
             Beer On Now
             {_.map(beerOnNow, (beer, beer_idx) => (
                 
@@ -19,9 +22,12 @@ export default class DrinksList extends React.Component {
                     {beer.beer_name} by {beer.beer_brewery}
                     {beer.beer_abv}%
                     £{beer.beer_price}
-                    {console.log("beer",beer)}
+                    On Now:{beer.beer_on_now}
+                    Coming Soon:{beer.beer_coming_soon}
                 </div>
             ))}
+            </div>
+            <div className="beer_coming_soon">
             Beer Coming Soon
             {_.map(beerComingSoon, (beer, beer_idx) => (
                 
@@ -29,9 +35,12 @@ export default class DrinksList extends React.Component {
                     {beer.beer_name} by {beer.beer_brewery}
                     {beer.beer_abv}%
                     £{beer.beer_price}
+                    On Now:{beer.beer_on_now}
+                    Coming Soon:{beer.beer_coming_soon}
                     {console.log("beer",beer)}
                 </div>
             ))}
+            </div>
             </Layout>
         );
     }

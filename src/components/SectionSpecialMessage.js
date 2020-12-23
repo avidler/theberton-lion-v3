657c920a78310dab5bfb767e52fa8e1d5e698e17
Adam Vidler <a.vidler@live.co.uk>
2020-12-23 15:13:13 +0000

    december23 update

--- src/components/SectionSpecialMessage.js
+++ src/components/SectionSpecialMessage.js
@@ -10,9 +10,7 @@ export default class SectionSpecialMessage extends React.Component {
         <section id={_.get(this.props, 'section.section_id')} className={'wrapper alt ' + _.get(this.props, 'section.section_id')}>
             <div className="container" id="special_message">
             <h2 className="page-title">{`${section.title}`}</h2>
-            
-            <p>December Update 23/12/20</p>
-
+           
 <p>Following much soul searching, speaking to others, looking at latest figures for the region and judging the general mood, we have decided to close The Theberton Lion for eat in service from TOMORROW NIGHT at 10pm for the time being. We will still be offering takeaway beer daily, (including 12pm-1pm on Christmas Day!), and takeaway food will be available from Monday 28th - Thu 31 December. We want everyone to stay safe and look forward to welcoming you back in the pub as soon as we can. Have a lovely, if different, but most importantly safe Christmas.
 </p>
             <p>{`${section.content}`}</p>

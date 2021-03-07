e047b25648eaabefa87becfb6795f178802b6703
Adam Vidler <a.vidler@live.co.uk>
2021-03-07 16:30:54 +0000

    food ordering buttons

--- src/templates/food.js
+++ src/templates/food.js
@@ -230,8 +230,8 @@ for (var i = 0; i < btns.length; i++) {
                 <span className="food_header_nav">
                 <nav id="menuNav" className="menuNav">
                
-<span className="glf-button" data-glf-cuid="50c453a2-d190-4cc1-83dd-d2daae9a31e2" data-glf-ruid="66c5d05f-76ff-468c-9142-d082e559d3b9"  > See MENU & Order</span>
-<span className="glf-button reservation" data-glf-cuid="50c453a2-d190-4cc1-83dd-d2daae9a31e2" data-glf-ruid="66c5d05f-76ff-468c-9142-d082e559d3b9" data-glf-reservation="true" > Table Reservation</span>
+<span className="glf-button" data-glf-cuid="50c453a2-d190-4cc1-83dd-d2daae9a31e2" data-glf-ruid="66c5d05f-76ff-468c-9142-d082e559d3b9"  > Order Takeaway</span>
+<span className="glf-button reservation" data-glf-cuid="50c453a2-d190-4cc1-83dd-d2daae9a31e2" data-glf-ruid="66c5d05f-76ff-468c-9142-d082e559d3b9" data-glf-reservation="true" > Book a Table</span>
 
                         <button className="btn active" onClick={() => setMenuChoice("specials")}>Specials </button>
                         <button className="btn" onClick={() => setMenuChoice("mains")}>Mains</button>

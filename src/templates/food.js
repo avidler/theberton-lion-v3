f36dc306738f97f5f32b00e245f25a20b904a0ae
Adam Vidler <a.vidler@live.co.uk>
2021-03-07 15:39:55 +0000

    food ordering buttons

--- src/templates/food.js
+++ src/templates/food.js
@@ -6,6 +6,12 @@ import {Layout} from '../components/index';
 
 
 function FoodMenu (props) {
+    const script = document.createElement("script");
+    script.src = "https://www.fbgcdn.com/embedder/js/ewm2.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+
     const [menuChoice, setMenuChoice] = useState("specials") 
     
         //const allData = _.get(props, 'pageContext.site.data')
@@ -223,7 +229,7 @@ for (var i = 0; i < btns.length; i++) {
                 <span className="food_header_nav">
                 <nav id="menuNav" className="menuNav">
                
-<span className="glf-button" data-glf-cuid="50c453a2-d190-4cc1-83dd-d2daae9a31e2" data-glf-ruid="66c5d05f-76ff-468c-9142-d082e559d3b9" > See MENU & Order</span>
+<span className="glf-button" data-glf-cuid="50c453a2-d190-4cc1-83dd-d2daae9a31e2" data-glf-ruid="66c5d05f-76ff-468c-9142-d082e559d3b9"  > See MENU & Order</span>
 <span className="glf-button reservation" data-glf-cuid="50c453a2-d190-4cc1-83dd-d2daae9a31e2" data-glf-ruid="66c5d05f-76ff-468c-9142-d082e559d3b9" data-glf-reservation="true" > Table Reservation</span>
 
                         <button className="btn active" onClick={() => setMenuChoice("specials")}>Specials </button>

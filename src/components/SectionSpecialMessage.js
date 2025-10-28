080cc3bb1085cd4d9c3d9f643a2bc2d3da64898d
avidler <a.vidler@live.co.uk>
2025-10-28 15:34:03 +0000

    checking updates work

--- src/components/SectionSpecialMessage.js
+++ src/components/SectionSpecialMessage.js
@@ -6,7 +6,7 @@ export default class SectionSpecialMessage extends React.Component {
     
     render() {
         let section = _.get(this.props, 'section');
-        const dates = ["Tue - Thu 12.00pm - 2.00pm  - Drinks Only",<br />,
+        const dates = ["Tue - Thu 12.00pm - 2.00pm - Drinks Only",<br />,
             "Friday 12.00pm - 3.00pm & 5.00pm - 9.00pm :: Food 12.00pm - 3.00pm & 5.00pm - 9.00pm",<br />,
             "Saturday 12.00pm - 9.00pm :: Food 12.00pm - 9.00pm",<br />,
             "Sunday 12.00pm - 6.00pm :: Food 12.00pm - 5.00pm"]

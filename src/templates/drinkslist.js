aa84fc10499a89ab449d218f3287cd9d9b48799b
avidler <a.vidler@live.co.uk>
2025-10-28 16:51:01 +0000

    drinks page

--- src/templates/drinkslist.js
+++ src/templates/drinkslist.js
@@ -423,8 +423,14 @@ for (var i = 0; i < btns.length; i++) {
             <Layout {...props}>
                  <div className="container">
                 <div className="drink_header">
-               <iframe src="https://business.untappd.com/embeds/iframes/50115/175853" frameborder="0" height="2000" width="100%"></iframe>
+                <span className="drink_header_title"></span>
+                <span className="drink_header_nav">
+                <nav id="menuNav" className="menuNav">
+                <iframe src="https://business.untappd.com/embeds/iframes/50115/175853" frameborder="0" height="2000" width="100%"></iframe>
                
+                   
+                </nav>
+                </span>
                 </div>
                 
             </div>

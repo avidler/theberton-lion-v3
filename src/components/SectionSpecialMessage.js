7f2242a63d5b52ea86c16097ec567cf2a8f780c8
avidler <a.vidler@live.co.uk>
2025-11-11 17:50:50 +0000

    xmas poster

--- src/components/SectionSpecialMessage.js
+++ src/components/SectionSpecialMessage.js
@@ -12,7 +12,7 @@ export default class SectionSpecialMessage extends React.Component {
             "Sunday 12.00pm - 6.00pm :: Food 12.00pm - 5.00pm"]
     return (
         <section id={_.get(this.props, 'section.section_id')} className={'wrapper alt ' + _.get(this.props, 'section.section_id')}>
-        
+        <img src="images/christmasmenuposter2025.jpg" alt="christmas 2025" />
         </section>
     )
 }
